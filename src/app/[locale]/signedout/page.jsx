a1a057304fdfaa86c1acb749d1d35ba2b064bf13
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-02-24 03:17:01 +0300

    Files updated

--- src/app/[locale]/signedout/page.jsx
+++ src/app/[locale]/signedout/page.jsx
@@ -1,40 +1,35 @@
 "use client";
 import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { signOut, useSession } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
-import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const Signedout = () => {
-    const router = useSearchParams();
-    const msg = router.get('msg');
     const t = useTranslations('signedout');
+    const { status } = useSession();
+    const router = useRouter();
+    useEffect(() => {
+        if (status === 'authenticated') {
+            signOut({ redirect: false })
+                .then(() => {
+                    router.push('/signedout');
+                });
+        } else {
+            router.push('/signedout');
+        }
+    }, [status, router]);
     return (
         <>
-            {
-                msg === "YouDontHaveAccount" ? (
-                    <>
-                        <div className="flex justify-center">
-                            <div className="px-16 py-14 rounded-full outline-offset-2 outline-4 outline-red-600 outline">
-                                <FontAwesomeIcon icon={faXmark} size={`3x`} style={{ color: `#dc2626` }} />
-                            </div>
-                        </div>
-                        <div className='flex justify-center mt-6'>
-                            <h2>{t('you_dont_have_account')}</h2>
-                        </div>
-                    </>
-                ) : (
-                    <>
-                        <div className="flex justify-center">
-                            <div className="px-16 py-16 rounded-full outline-offset-2 outline-4 outline-green-600 outline">
-                                <FontAwesomeIcon icon={faCheck} size={`3x`} style={{ color: `#16a34a` }} />
-                            </div>
-                        </div>
-                        <div className='flex justify-center mt-6'>
-                            <h2>{t('signedout_successfully')}</h2>
-                        </div>
-                    </>
-                )
-            }
+            <div className="flex justify-center">
+                <div className="px-16 py-16 rounded-full outline-offset-2 outline-4 outline-green-600 outline">
+                    <FontAwesomeIcon icon={faCheck} size={`3x`} style={{ color: `#16a34a` }} />
+                </div>
+            </div>
+            <div className='flex justify-center mt-6'>
+                <h2>{t('signedout_successfully')}</h2>
+            </div>
         </>
     )
 }
