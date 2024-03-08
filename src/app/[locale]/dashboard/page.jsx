e532e00f006589484a758602ebc3b9805a38cd31
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-03-09 01:33:47 +0300

    files modified & renamed

--- src/app/[locale]/dashboard/page.jsx
+++ src/app/[locale]/dashboard/page.jsx
@@ -4,7 +4,6 @@ import { faClockRotateLeft, faGear, faMobileScreen, faShield, faUser } from '@fo
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import MyAccount from '../../../components/dashboard/profile.MyAccount';
 import { useRouter } from 'next/navigation';
-import Security from '../../../components/dashboard/profile.Security';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../../firebase';
 import { useEffect } from 'react';
@@ -22,16 +21,12 @@ const ProfileNvabar = () => {
             <Tabs variant='unstyled' align='center' className='-mt-[40px]'>
                 <TabList bgColor="#242424" height="60px" className="overflow-x-auto overflow-y-hidden w-full">
                     <Tab _selected={{ color: 'white', bg: '#343434' }} _hover={{ color: 'white', bg: '#343434' }}><FontAwesomeIcon icon={faUser} className='ltr:mr-2 rtl:ml-2' />My Account</Tab>
-                    <Tab _selected={{ color: 'white', bg: '#343434' }} _hover={{ color: 'white', bg: '#343434' }}><FontAwesomeIcon icon={faShield} className='ltr:mr-2 rtl:ml-2' />Security</Tab>
                     <Tab _selected={{ color: 'white', bg: '#343434' }} _hover={{ color: 'white', bg: '#343434' }}><FontAwesomeIcon icon={faClockRotateLeft} className='ltr:mr-2 rtl:ml-2' />Activity</Tab>
                     <Tab _selected={{ color: 'white', bg: '#343434' }} _hover={{ color: 'white', bg: '#343434' }}><FontAwesomeIcon icon={faGear} className='ltr:mr-2 rtl:ml-2' />Settings</Tab>
                 </TabList>
                 <TabPanels className='mt-[40px]'>
                     <TabPanel>
                         <MyAccount />
-                    </TabPanel>
-                    <TabPanel>
-                        <Security />
                     </TabPanel>
                     <TabPanel>
 
--- src/components/Provider/index.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-export default function NextAuthProvider({ children, session }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
-};
