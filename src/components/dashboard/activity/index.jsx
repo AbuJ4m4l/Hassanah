ad0699bb8701bf8e66eae8d1099fb9cae64c30d6
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-03-10 01:25:03 +0300

    files modified

--- src/components/dashboard/activity/index.jsx
+++ src/components/dashboard/activity/index.jsx
@@ -39,6 +39,7 @@ const Activity = () => {
                                                     <div className="ltr:-ml-[25px] ltr:float-right rtl:float-left">
                                                         <button className="rounded-full bg-red-600 p-4">
                                                             <FontAwesomeIcon icon={faTrashCan} size='2xl' />
+
                                                         </button>
                                                     </div>
                                                     <Image
