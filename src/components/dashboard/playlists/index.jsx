5076fe50d36e61e74e180b7f589c7e037d0591a4
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-03-13 18:00:24 +0300

    files modified

--- src/components/dashboard/playlists/index.jsx
+++ src/components/dashboard/playlists/index.jsx
@@ -1,153 +1,91 @@
 "use client";
 
-import { useRef, useState } from "react";
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsisVertical, faEye, faPencil, faPlus } from "@fortawesome/free-solid-svg-icons";
-import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    FormControl,
-    FormLabel,
-    Input,
-    useDisclosure,
-    ChakraProvider,
-} from '@chakra-ui/react';
-import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
-import Image from "next/image";
-import theme from "../../../commonTheme";
-import { useTranslations } from "next-intl";
+import React from "react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, getKeyValue} from "@nextui-org/react";
+import {columns, users} from "./data";
 
-const Playlists = () => {
-    const t = useTranslations('activity');
-    const [playlistName, setPlaylistNameInput] = useState('');
-    const [playlistDescription, setPlaylistDesciptionInput] = useState('');
-    const [EditPlaylistName, setEditPlaylistNameInput] = useState('');
-    const [EditPlaylistDescription, setEditPlaylistDesciptionInput] = useState('');
-    const {
-        isOpen: ModalIsOpen,
-        onOpen: ModalOnOpen,
-        onClose: ModalOnClose
-    } = useDisclosure();
+const statusColorMap = {
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
+};
 
-    const {
-        isOpen: EditModalIsOpen,
-        onOpen: EditModalOnOpen,
-        onClose: EditModalOnClose
-    } = useDisclosure();
-    const initialRef = useRef(null);
-    const finalRef = useRef(null);
+const PlayLists = () => {
+  const renderCell = React.useCallback((user, columnKey) => {
+    const cellValue = user[columnKey];
 
-    const createNewPlaylist = () => {
-        /* TODO: create new function that creates newPlaylist */
-    };
-    return (
-        <ChakraProvider theme={theme}>
-            <div className="mt-4 flex flex-col">
-                <div>
-                    <button onClick={ModalOnOpen} className="rounded-full bg-secondry px-6 py-4 rtl:float-right ltr:float-left"><FontAwesomeIcon icon={faPlus} /></button>
-                </div>
-                <Modal
-                    initialFocusRef={initialRef}
-                    finalFocusRef={finalRef}
-                    isOpen={ModalIsOpen}
-                    onClose={ModalOnClose}
-                >
-                    <ModalOverlay />
-                    <ModalContent>
-                        <ModalHeader>{t('create_playlist')}</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody pb={6}>
-                            <FormControl>
-                                <FormLabel>{t('playlist_name')}</FormLabel>
-                                <Input ref={initialRef} placeholder={t('playlist_name')} variant="filled" value={playlistName} onChange={(e) => setPlaylistNameInput(e?.target?.value)} />
-                            </FormControl>
-
-                            <FormControl mt={4}>
-                                <FormLabel>{t('playlist_description')}</FormLabel>
-                                <Input variant='filled' placeholder={t('playlist_description')} value={playlistDescription} onChange={(e) => setPlaylistDesciptionInput(e?.target?.value)} />
-                            </FormControl>
-                        </ModalBody>
-
-                        <ModalFooter>
-                            <button onClick={createNewPlaylist} className="btn mr-3 bg-primary">
-                                {t('create')}
-                            </button>
-                            <button onClick={ModalOnClose}>{t('cancel')}</button>
-                        </ModalFooter>
-                    </ModalContent>
-                </Modal>
-
-                <div className="mt-5">
-                    <div className="mx-5 bg-secondry-light rounded-lg mt-6">
-                        <div className="h-auto">
-                            <div className="flex flex-col space-x-5 p-10">
-                                <div className="inline-block">
-                                    <ul className="space-y-6">
-                                        <li className="flex-row flex space-x-4 no-underline hover:underline decoration-2 decoration-primary">
-                                            <Image src={"/images/reciters/yasser-al-dosry.png"} alt="rectier photo" width={75} height={125} loading="lazy" className="rounded-md" />
-                                            <Link href="#">
-                                                XXX (Playlist)
-                                            </Link>
-                                            <Menu>
-                                                <MenuButton className="rtl:mr-4 ltr:ml-4 -translate-y-5">
-                                                    <FontAwesomeIcon icon={faEllipsisVertical} />
-                                                </MenuButton>
-                                                <MenuList>
-                                                    <MenuItem onClick={EditModalOnOpen}>{t('edit')} <FontAwesomeIcon className="ltr:ml-2 rtl:mr-2" icon={faPencil} /></MenuItem>
-                                                    <MenuItem>{t('view')} <FontAwesomeIcon className="ltr:ml-2 rtl:mr-2" icon={faEye} /></MenuItem>
-                                                    <MenuItem>{t('delete')} <FontAwesomeIcon className="ltr:ml-2 rtl:mr-2" icon={faTrashCan} /></MenuItem>
-                                                </MenuList>
-                                            </Menu>
-                                            <Modal
-                                                initialFocusRef={initialRef}
-                                                finalFocusRef={finalRef}
-                                                isOpen={EditModalIsOpen}
-                                                onClose={EditModalOnClose}
-                                            >
-                                                <ModalOverlay />
-                                                <ModalContent>
-                                                    <ModalHeader>{t('edit_playlist')}</ModalHeader>
-                                                    <ModalCloseButton />
-                                                    <ModalBody pb={6}>
-                                                        <FormControl>
-                                                            <FormLabel>{t('edit_playlist_name')}</FormLabel>
-                                                            <Input ref={initialRef} placeholder={t('edit_playlist_name')} variant="filled" value={EditPlaylistName} onChange={(e) => setEditPlaylistNameInput(e?.target?.value)} />
-                                                        </FormControl>
-
-                                                        <FormControl mt={4}>
-                                                            <FormLabel>{t('edit_playlist_description')}</FormLabel>
-                                                            <Input variant='filled' placeholder={t('edit_playlist_description')} value={EditPlaylistDescription} onChange={(e) => setEditPlaylistDesciptionInput(e?.target?.value)} />
-                                                        </FormControl>
-                                                    </ModalBody>
-
-                                                    <ModalFooter>
-                                                        <button onClick={createNewPlaylist} className="btn mr-3 bg-primary">
-                                                            {t('save')}
-                                                        </button>
-                                                        <button onClick={EditModalOnClose}>{t('cancel')}</button>
-                                                    </ModalFooter>
-                                                </ModalContent>
-                                            </Modal>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    switch (columnKey) {
+      case "name":
+        return (
+           <>
+            <div>
+                <button onClick={ModalOnOpen} className="rounded-full bg-secondry px-6 py-4 rtl:float-right ltr:float-left"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
             </div>
-        </ChakraProvider>
-    )
-}
+          <User
+            avatarProps={{radius: "lg", src: user.avatar}}
+            description={user.email}
+            name={cellValue}
+          >
+            {user.email}
+          </User>
+          </>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+          </div>
+        );
+      case "status":
+        return (
+            <>
+          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+            {cellValue}
+          </Chip>
+          </>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
 
-export default Playlists;
+  return (
+  <Table aria-label="Example table with custom cells">
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody items={users}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
