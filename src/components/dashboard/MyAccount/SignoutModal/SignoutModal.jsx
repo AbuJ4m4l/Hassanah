030e50efba62aa59374308f2b165363742d8caf4
abuj4m4l <msgamal563@gmail.com>
2024-03-18 21:49:11 +0000

    files modified

---
+++ src/components/dashboard/MyAccount/SignoutModal/SignoutModal.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+  Button,
+  Link,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@nextui-org/react";
+import { useTranslations } from "next-intl";
+
+const SignoutModal = ({ isOpen, onOpenChange, Playlist }) => {
+  const t = useTranslations("SignoutModal");
+  return (
+    <Modal className="rtl:ltr" isOpen={isOpen} onOpenChange={onOpenChange}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              {t("title")}
+            </ModalHeader>
+            <ModalBody>
+              {t("attention")}
+            </ModalBody>
+            <ModalFooter>
+              <Button color="default" variant="light" onPress={onClose}>
+                {t("cancel")}
+              </Button>
+              <Button href="/signout" as={Link} className="!text-white" color="danger" onPress={onClose}>
+                {t("signout")}
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default SignoutModal;
