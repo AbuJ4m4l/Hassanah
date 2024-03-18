c61fb664b59dbe8a1c4b03b3e42e51a753b133ce
msgamal563 <msgamal563@gmail.com>
2024-03-18 18:05:43 +0000

    files modified

--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
-const withNextIntl = require('next-intl/plugin')();
+const withNextIntl = require("next-intl/plugin")();
 
 module.exports = withNextIntl({
   reactStrictMode: false,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '443',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "443",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'graph.facebook.com',
-        port: '443',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "graph.facebook.com",
+        port: "443",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-        port: '',
-        pathname: '/vi/**',
-      }
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+        pathname: "/vi/**",
+      },
     ],
   },
-  poweredByHeader: false
+  poweredByHeader: false,
 });
