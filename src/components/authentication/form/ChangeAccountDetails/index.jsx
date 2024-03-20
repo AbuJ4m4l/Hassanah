54991d27294e3647c2f524ea6ad7da06e961264a
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-03-20 18:32:57 +0300

    files modified

--- src/components/authentication/form/ChangeAccountDetails/index.jsx
+++ src/components/authentication/form/ChangeAccountDetails/index.jsx
@@ -27,7 +27,7 @@ const ChangeAccountDetailsForm = ({ locale, className }) => {
     if (password === "") return true;
     return false;
   }, [password]);
-  const handleForm = (e) => {
+  const handleVerificationPasswordForm = (e) => {
     try {
       e.preventDefault();
       if (email && password) {
@@ -40,6 +40,7 @@ const ChangeAccountDetailsForm = ({ locale, className }) => {
           });
       }
     } catch (error) {
+      setIsAccessGranted(false);
       console.error(error);
     }
   };
@@ -119,7 +120,7 @@ const ChangeAccountDetailsForm = ({ locale, className }) => {
             </Button>
           </form>
         </>
-      ) : (
+      ) : locale === "ar" ? (
         <>
           <Input
             isRequired
@@ -192,7 +193,92 @@ const ChangeAccountDetailsForm = ({ locale, className }) => {
             }
             type={showPassword ? "text" : "password"}
           />
-          <Button className="mt-6" color="primary" onClick={handleForm}>
+          <Button
+            className="mt-6"
+            color="primary"
+            onClick={handleVerificationPasswordForm}
+          >
+            {t("verify_password")}
+          </Button>
+        </>
+      ) : (
+        <>
+          <Input
+            isRequired
+            name="password"
+            label={t("password")}
+            placeholder={t("enter_your_password")}
+            value={password}
+            isInvalid={isPasswordInvalid}
+            color={isPasswordInvalid ? "danger" : "default"}
+            errorMessage={isPasswordInvalid && t("password_wrong")}
+            onValueChange={setPassword}
+            autoComplete="current-password"
+            startContent={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-lock"
+              >
+                <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+              </svg>
+            }
+            endContent={
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-eye"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-eye-off"
+                  >
+                    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
+                    <line x1="1" y1="1" x2="23" y2="23"></line>
+                  </svg>
+                )}
+              </button>
+            }
+            type={showPassword ? "text" : "password"}
+          />
+          <Button
+            className="mt-6"
+            color="primary"
+            onClick={handleVerificationPasswordForm}
+          >
             {t("verify_password")}
           </Button>
         </>
