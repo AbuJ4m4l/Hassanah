f903882c19015fbe02ce12aaecb544f8e3779934
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-03-20 03:24:42 +0300

    files modified

--- src/components/authentication/pages/reset-password/index.jsx
+++ src/components/authentication/pages/reset-password/index.jsx
@@ -2,12 +2,17 @@
 
 import { useSearchParams } from "next/navigation";
 import SendResetPasswordEmailClientComponent from "../../form/ResetPasswordRequest";
+import { useTranslations } from "next-intl";
 
 const ResetPasswordClientComponent = ({ locale }) => {
     const params = useSearchParams();
+    const t = useTranslations('resetPassword');
     return (
         <>
-            <SendResetPasswordEmailClientComponent emailFromQuery={params.get('email') ? params.get('email') : ""} locale={locale} className="flex flex-col mx-auto items-center w-[340px]" />
+            <div className="flex justify-center">
+                <h1>{t('resetPassword')}</h1>
+            </div>
+            <SendResetPasswordEmailClientComponent emailFromQuery={params.get('email') ? params.get('email') : ""} locale={locale} className="flex flex-col mt-10 mx-auto items-center w-[340px]" />
         </>
     )
 }
