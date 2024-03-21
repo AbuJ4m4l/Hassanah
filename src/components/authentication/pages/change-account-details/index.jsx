fcf717f0ec389c926f2296c7f2d3a07b86a00df2
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-03-21 16:16:00 +0300

    files modified

--- src/components/authentication/pages/change-account-details/index.jsx
+++ src/components/authentication/pages/change-account-details/index.jsx
@@ -1,7 +1,20 @@
 "use client";
+import { useAuthState } from "react-firebase-hooks/auth";
 import ChangeAccountDetailsForm from "../../form/ChangeAccountDetails";
+import { auth } from "../../../../lib/firebase";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const ChangeAccountDetailsClientPage = ({ locale }) => {
+  const router = useRouter();
+  const [user, loading] = useAuthState(auth);
+  useEffect(() => {
+    if (loading === false) {
+      if (!user?.email) {
+        router.push("/signup");
+      }
+    }
+  });
   return (
     <ChangeAccountDetailsForm
       locale={locale}
