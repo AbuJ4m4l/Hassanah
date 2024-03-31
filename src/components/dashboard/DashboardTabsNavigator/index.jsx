eda17cd3721fac88084a14e45bfb718b48d59c9e
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-03-31 09:56:56 +0300

    Bugs Fixes

--- src/components/dashboard/DashboardTabsNavigator/index.jsx
+++ src/components/dashboard/DashboardTabsNavigator/index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Tabs, Tab } from "@nextui-org/react";
+import { Tabs, Tab, useDisclosure } from "@nextui-org/react";
 import MyAccount from "../MyAccount";
 import { useRouter } from "next/navigation";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -9,71 +9,114 @@ import Activity from "../activity";
 import Playlists from "../playlists";
 import Settings from "../settings";
 import { auth } from "../../../lib/firebase";
+import AccessDeniedModal from "../MyAccount/AccessDeniedModal/AccessDeniedModal";
 
 const DashboardTabsNavigator = ({ locale }) => {
-  const router = useRouter();
   const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+  const [isAccessGranted, setIsAccessGranted] = useState();
   const t = useTranslations("dashboard");
+  const {
+    isOpen: isAccessDeniedModalOpen,
+    onOpen: onAccessDeniedModalOpen,
+    onOpenChange: onAccessDeniedOpenChange,
+    onClose: onAccessDeniedModalClose,
+  } = useDisclosure();
   useEffect(() => {
-    if (loading === false) {
-      if (!user?.email) {
-        router.push("/signup");
+    const accessGranted = async () => {
+      if (loading === false) {
+        if (!user?.email) {
+          router.push("/signup");
+          return;
+        }
+        if (user?.providerId === "password" && user?.emailVerified === false) {
+          setIsAccessGranted(false);
+          onAccessDeniedModalOpen();
+        } else if (
+          user?.emailVerified === true ||
+          user?.providerId === "facebook.com" ||
+          user?.providerId === "google.com"
+        ) {
+          setIsAccessGranted(true);
+          onAccessDeniedModalClose();
+        }
       }
-    }
-  });
+    };
+
+    accessGranted();
+  }, [
+    loading,
+    router,
+    user?.email,
+    user?.providerId,
+    user?.emailVerified,
+    onAccessDeniedModalOpen,
+  ]);
   const [selected, setSelected] = useState(t("myAccount"));
+  console.log(isAccessGranted);
   return (
     <>
-      <div className="flex w-full flex-col">
-        <Tabs
-          radius="full"
-          aria-label="Options"
-          selectedKey={selected}
-          onSelectionChange={setSelected}
-          className="flex justify-center mx-4 md:mx-0"
-        >
-          <Tab
-            key="myAccount"
-            title={
-              <div className="flex items-center ltr:space-x-2">
-                <span>{t("myAccount")}</span>
-              </div>
-            }
-          >
-            <MyAccount locale={locale} />
-          </Tab>
-          <Tab
-            key="playlists"
-            title={
-              <div className="flex items-center space-x-2">
-                <span>{t("playlists")}</span>
-              </div>
-            }
-          >
-            <Playlists />
-          </Tab>
-          <Tab
-            key="activity"
-            title={
-              <div className="flex items-center space-x-2">
-                <span>{t("activity")}</span>
-              </div>
-            }
-          >
-            <Activity />
-          </Tab>
-          <Tab
-            key="settings"
-            title={
-              <div className="flex items-center space-x-2">
-                <span>{t("settings")}</span>
-              </div>
-            }
-          >
-            <Settings />
-          </Tab>
-        </Tabs>
-      </div>
+      {isAccessGranted === true ? (
+        <>
+          <div className="flex w-full flex-col">
+            <Tabs
+              radius="full"
+              aria-label="Options"
+              selectedKey={selected}
+              onSelectionChange={setSelected}
+              className="flex justify-center mx-4 md:mx-0"
+            >
+              <Tab
+                key="myAccount"
+                title={
+                  <div className="flex items-center ltr:space-x-2">
+                    <span>{t("myAccount")}</span>
+                  </div>
+                }
+              >
+                <MyAccount locale={locale} />
+              </Tab>
+              <Tab
+                key="playlists"
+                title={
+                  <div className="flex items-center space-x-2">
+                    <span>{t("playlists")}</span>
+                  </div>
+                }
+              >
+                <Playlists />
+              </Tab>
+              <Tab
+                key="activity"
+                title={
+                  <div className="flex items-center space-x-2">
+                    <span>{t("activity")}</span>
+                  </div>
+                }
+              >
+                <Activity />
+              </Tab>
+              <Tab
+                key="settings"
+                title={
+                  <div className="flex items-center space-x-2">
+                    <span>{t("settings")}</span>
+                  </div>
+                }
+              >
+                <Settings />
+              </Tab>
+            </Tabs>
+          </div>
+        </>
+      ) : (
+        <>
+          <AccessDeniedModal
+            isOpen={true}
+            onOpenChange={onAccessDeniedOpenChange}
+          />
+        </>
+      )}
     </>
   );
 };
--- src/components/dashboard/MyAccount/index.jsx
+++ src/components/dashboard/MyAccount/index.jsx
@@ -23,16 +23,7 @@ const MyAccount = ({ locale }) => {
     onOpen: onSignoutModalOpen,
     onOpenChange: onSignoutOpenChange,
   } = useDisclosure();
-  const {
-    isOpen: isAccessDeniedModalOpen,
-    onOpen: onAccessDeniedModalOpen,
-    onOpenChange: onAccessDeniedOpenChange,
-  } = useDisclosure();
-  useEffect(() => {
-    if (user?.emailVerified === false) {
-      onAccessDeniedModalOpen();
-    }
-  }, [user?.emailVerified, onAccessDeniedModalOpen]);
+
   return (
     <section role="section" className="mt-8">
       <div className="mt-6 flex justify-center">
@@ -230,10 +221,6 @@ const MyAccount = ({ locale }) => {
       <SignoutModal
         isOpen={isSignoutModalOpen}
         onOpenChange={onSignoutOpenChange}
-      />
-      <AccessDeniedModal
-        isOpen={isAccessDeniedModalOpen}
-        onOpenChange={onAccessDeniedOpenChange}
       />
     </section>
   );
