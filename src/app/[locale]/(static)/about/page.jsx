2db95ada973d403c04a7daee5bbc03ec35216348
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-04-03 18:04:24 +0300

    Files Modified Commit

--- src/app/[locale]/(static)/about/page.jsx
+++ src/app/[locale]/(static)/about/page.jsx
@@ -12,17 +12,21 @@ const AboutPage = ({ params: { locale } }) => {
 
   return (
     <div className="container !-mt-8">
-      <section className="bg-primary w-full h-[500px]">
-        <div>
-          <div className="w-[280px]">
-            <h1
-              className={`text-5xl ${
-                locale === "en" ? russo.className : changa.className
-              }`}
-            >
-              {t("heroSection.heading")}
-            </h1>
-            <p className="mt-4">{t("heroSection.text")}</p>
+      <section className="bg-primary px-10 py-8 xl:px-[300px] lg:py-16 w-full h-auto">
+        <div className="flex flex-col md:flex-row">
+          <div className="w-[280px] flex flex-col">
+            <div classname="flex justify-center">
+              <h1
+                className={`text-5xl 2xl:text-7xl ${
+                  locale === "en" ? russo.className : changa.className
+                }`}
+              >
+                {t("heroSection.heading")}
+              </h1>
+            </div>
+            <div classname="flex justify-center">
+              <p className="mt-4">{t("heroSection.text")}</p>
+            </div>
           </div>
           <Image
             as={nextImage}
@@ -30,7 +34,8 @@ const AboutPage = ({ params: { locale } }) => {
             height={300}
             alt="Connected"
             loading="lazy"
-            src="/images/svg/connected"
+            src="/images/svg/connected.svg"
+            className="md:rtl:mr-28 md:ltr:ml-28 mt-9"
           />
         </div>
       </section>
