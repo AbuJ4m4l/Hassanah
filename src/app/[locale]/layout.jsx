cc3b76e63af0c41ef93f8178cf4512dbb207e884
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-04-04 18:11:03 +0300

    Files Modified Commit

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -122,6 +122,28 @@ export default async function RootLayout({ children, params: { locale } }) {
                 <Navbar locale={locale} />
                 <div className="container">
                   <main className="content mb-[60px] mt-[40px] flex-1">
+                    <div
+                      aria-hidden="true"
+                      className="select-none fixed hidden dark:md:block dark:opacity-70 -top-[80%] -right-[60%] 2xl:-top-[60%] 2xl:-right-[45%] z-0 rotate-12"
+                    >
+                      <img
+                        src="/gradients/docs-right.png"
+                        className="relative z-10 opacity-0 shadow-black/5 data-[loaded=true]:opacity-100 shadow-none transition-transform-opacity motion-reduce:transition-none !duration-300 rounded-large"
+                        alt="right background"
+                        data-loaded="true"
+                      />
+                    </div>
+                    <div
+                      aria-hidden="true"
+                      className="select-none fixed hidden dark:md:block dark:opacity-70 -bottom-[40%] -left-[20%] z-0"
+                    >
+                      <img
+                        src="/gradients/docs-left.png"
+                        className="relative z-10 opacity-0 shadow-black/5 data-[loaded=true]:opacity-100 shadow-none transition-transform-opacity motion-reduce:transition-none !duration-300 rounded-large"
+                        alt="left background"
+                        data-loaded="true"
+                      />
+                    </div>
                     {children}
                   </main>
                   <Footer locale={locale} />
