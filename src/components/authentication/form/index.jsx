e911bccf742199abb0f3b0d2a6ca4a58a34a1719
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-04-16 01:10:05 +0300

    Files Modified Commit

--- src/components/authentication/form/index.jsx
+++ src/components/authentication/form/index.jsx
@@ -4,11 +4,13 @@ import SignupForm from "./SignupForm";
 import LoginForm from "./LoginForm";
 import { useState } from "react";
 import { NextSeo } from "next-seo";
+import { useTheme } from "next-themes";
 import { useTranslations } from "next-intl";
 
 const UserForm = ({ locale, tab }) => {
   const [selected, setSelected] = useState(tab);
   const t = useTranslations("user-form");
+  const { theme } = useTheme();
   return (
     <>
       <div className="flex flex-col w-full mx-auto items-center">
