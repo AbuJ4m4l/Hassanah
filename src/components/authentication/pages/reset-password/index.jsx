9bbbfba8c13fe23babb62f7d5c4fd4eb5c239739
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-04-16 01:25:12 +0300

    Files Modified Commit

--- src/components/authentication/pages/reset-password/index.jsx
+++ src/components/authentication/pages/reset-password/index.jsx
@@ -1,14 +1,21 @@
 "use client";
-
+import { NextSeo } from "next-seo";
+import { useTheme } from "next-themes";
 import { useSearchParams } from "next/navigation";
 import SendResetPasswordEmailClientComponent from "../../form/ResetPasswordRequest";
 import { useTranslations } from "next-intl";
 
 const ResetPasswordClientComponent = ({ locale }) => {
   const params = useSearchParams();
+  const { theme } = useTheme();
   const t = useTranslations("resetPassword");
   return (
     <>
+      <NextSeo
+        title={t("metadata.title")}
+        description={t("metadata.description")}
+        themeColor={theme}
+      />
       <div className="flex justify-center">
         <h1 className="text-xl md:text-2xl">{t("resetPassword")}</h1>
       </div>
