7ddcc375c352f3ec3c7933372f5cfc142b036251
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-04-17 21:40:57 +0300

    Files Modified Commit

--- src/components/languageSwitcher/index.jsx
+++ src/components/languageSwitcher/index.jsx
@@ -43,7 +43,7 @@ const LanguageSwitcher = ({ color, variant, location, className }) => {
         </DropdownTrigger>
         <DropdownMenu aria-label="Language switcher" disabledKeys={["Turkish"]}>
           <DropdownItem
-            className="flex flex-row"
+            className="flex flex-row !text-foreground"
             key="Arabic"
             as={Link}
             href={`/ar/${location}`}
@@ -62,7 +62,7 @@ const LanguageSwitcher = ({ color, variant, location, className }) => {
             العربية
           </DropdownItem>
           <DropdownItem
-            className="flex flex-row"
+            className="flex flex-row !text-foreground"
             as={Link}
             href={`/en/${location}`}
             key="English"
@@ -81,7 +81,7 @@ const LanguageSwitcher = ({ color, variant, location, className }) => {
             English
           </DropdownItem>
           <DropdownItem
-            className="flex flex-row"
+            className="flex flex-row !text-foreground"
             as={Link}
             href={`/tr/${location}`}
             key="Turkish"
