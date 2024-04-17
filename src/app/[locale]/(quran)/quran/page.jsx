810da19c118f4a0d354de536fa6ebc0e0a120f9b
Iyad Almsara <156677836+AbuJ4m4l@users.noreply.github.com>
2024-04-17 22:04:15 +0300

    Files Modified Commit

--- src/app/[locale]/(quran)/quran/page.jsx
+++ src/app/[locale]/(quran)/quran/page.jsx
@@ -1,17 +1,20 @@
 import SurahsIndex from "../../../../components/surahsIndex";
 
+export const russo = Russo_One({ weight: ["400"], subsets: ["latin"] });
+export const changa = Changa({ weight: ["600"], subsets: ["arabic"] });
+
 const Quran = ({ params: { locale } }) => {
   return (
     <>
-     <div className="flex justify-center mt-4">
-          <h1
-            className={`${
-              locale === "en" ? russo.className : changa.className
-            } text-2xl mb-4`}
-          >
-            {t("surahs")}
-          </h1>
-        </div>
+      <div className="flex justify-center mt-4">
+        <h1
+          className={`${
+            locale === "en" ? russo.className : changa.className
+          } text-2xl mb-4`}
+        >
+          {t("surahs")}
+        </h1>
+      </div>
       <SurahsIndex locale={locale} />
     </>
   );
